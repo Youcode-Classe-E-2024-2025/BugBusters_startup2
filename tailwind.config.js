699a55d7fc9d0f582d36b6c1c5f87045dfd61887
Atig-Hamza <abderrahmanahlalay76@gmail.com>
2024-11-08 23:28:37 +0100

    Main-Add continuous horizontal scrolling for partnership logos with white background and spacing

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,15 @@ module.exports = {
       colors: {
         primary: '#171717',
       },
+      animation:{
+        "loop-scroll": "loop-scroll 50s linear infinite",
+        },
+        keyframes: {
+        "loop-scroll": {
+        from: {transform: "translateX(0)" },
+        to: {transform: "translateX(-100%)" },
+        },
+      },
     },
   },
   plugins: [],
