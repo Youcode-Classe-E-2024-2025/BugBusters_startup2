4329fa560bcfd88b518a0835a271f6cf987706a2
achraf <achrafsikal@gmail.com>
2024-11-11 13:15:20 +0100

    resolve some github problem

--- assets/js/script.js
+++ assets/js/script.js
@@ -342,64 +342,6 @@ if (!localStorage.getItem("productData")) { //if no local storage make one
     saveGamesToStorage();
 }
 
-// Object.keys(games)[Object.keys(games).length-1] gets the last key string in object
-function gameAdd(){
-    let gameIndex= Number(Object.keys(games)[Object.keys(games).length-1].substr(4)); // removes the first 4 string coverts the rest to a number
-    let newIndex = gameIndex;
-    newIndex++;
-    let newKey= "game"+newIndex;
-    let newGame={[newKey]:{
-        title: document.forms["addForm"]["addTitle"].value,
-        category: "Action-Adventure",
-        images:[document.forms["addForm"]["fileUpload"].value],
-        shortDescription: document.forms["addForm"]["addShortInfo"].value,
-        longDescription: document.forms["addForm"]["addLongDescription"].value,
-        price: document.forms["addForm"]["addPrice"].value,
-        discount: document.forms["addForm"]["addDescount"].value,
-        discountQuantity: 180}
-    }
-    Object.assign(games,newGame);
-    saveGamesToStorage()
-
-}
-
-// let text = localStorage.getItem("productData");
-// games = JSON.parse(text);
-
-// for(let n=1;n<=Object.keys(games).length;n++){
-//     document.getElementById("pmList").innerHTML+=`
-//                 <div class="flex bg-white text-black text-3xl rounded-3xl p-6 h-min">
-//                     <div class="justify-items-center">
-//                         <img class="ml-3" src="${games[Object.keys(games)[n-1]].images}" alt="" width="100">
-//                     </div>
-//                     <div class="min-w-[42rem] ml-6">
-//                         <div class="grid gap-3">
-//                             <div>
-//                                 <p class="text-3xl font-medium">${games[Object.keys(games)[n-1]].title}</p>
-//                                 <p class="text-xl">type:<span class="ml-2">${games[Object.keys(games)[n-1]].category}</span></p>
-//                                 <p class="text-xl">Price:<span class="ml-2">${games[Object.keys(games)[n-1]].price} $US</span></p>
-//                             </div>
-//                             <div class="flex text-xl gap-10">
-//                                 <button id="game${n}" onclick="delGame(this.id)" >Delete</button>
-//                                 <button>Edit</button>
-//                             </div>
-//                         </div>
-//                     </div>
-//                 </div>`
-// };
-function delGame(gamid) {
-    delete games[gamid];
-    saveGamesToStorage();
-    renderGameList();
-}
-function vGame(gamid){
-    document.forms["editForm"]["editTitle"].value =games[gamid].title;
-    document.forms["editForm"]["editShortInfo"].value =games[gamid].shortDescription;
-    document.forms["editForm"]["editLongDescription"].value =games[gamid].longDescription;
-    document.forms["editForm"]["editPrice"].value =games[gamid].price;
-    document.forms["editForm"]["editDescount"].value =games[gamid].discount;
-    
-}
 const burgerMenu = document.getElementById('burgerMenu');
 const mobileMenu = document.getElementById('mobileMenu');
 
